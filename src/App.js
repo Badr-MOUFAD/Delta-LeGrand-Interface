333ab4896493ef488860202918cddb44ae3a9907
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2020-12-24 01:42:11 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+
+export default function App(props) {
+
+    return (
+        <div>
+            <h1>Delta LeGrand</h1>
+            <Button color="primary">Hello Delta LeGrand</Button>
+        </div>
+    );
+}
